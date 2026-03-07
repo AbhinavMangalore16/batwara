1ef6232845ff35033b15339b69d35144478dd557
AbhinavMangalore16 <abhinavm16104@gmail.com>
2026-03-07 11:28:08 +0530

    ngrok warning issue fix

--- lib/api.ts
+++ lib/api.ts
@@ -6,6 +6,7 @@ export async function apiFetch(path: string, options: RequestInit = {}){
         credentials: "include",
         headers:{
             "Content-Type": "application/json",
+            "ngrok-skip-browser-warning": "true",
             ...options.headers,
         }, 
         ...options
